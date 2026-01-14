9af69053f4f020c6292b9fa7f0ab6c4121c54940
photog1500 <63972912+photog1500@users.noreply.github.com>
2026-01-13 22:26:29 -0600

    Update vite.config.ts
    UPDATED VITE.CONFIG.TS

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,17 @@
+
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 
 export default defineConfig({
+  base: '/', // important for Pages
   plugins: [react()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
   },
-  server: {
-    port: 3000,
-    strictPort: true,
-    host: true,
-    allowedHosts: true,
-  }
+  build: {
+    outDir: 'dist',
+  },
 });
